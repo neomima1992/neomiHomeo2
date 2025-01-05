dd535eea41b9b6089c1ebcb2ff82b52fa695a508
neomima1992 <danielsneomi@gmail.com>
2025-01-05 23:20:22 +0200

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import About from "./pages/Thanx";
+import About from "./pages/ThankYou";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/thanx" element={<Thanx />} />
+      <Route path="/thank-you" element={<ThankYou />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
